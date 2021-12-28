7dfcecf1adc324de1fc23145742f4a4686d467c2
Francesco Lerro <francesco.lerro@leitha.eu>
2021-12-28 14:55:05 +0100

    fix path in error message

--- bin/backup_messages.sh
+++ bin/backup_messages.sh
@@ -3,8 +3,10 @@
 BASE_DIR=`dirname $0`
 cd `dirname $BASE_DIR`
 
+pwd 
+
 if [ ! -d 'venv' ]; then
-  echo "Create virtualenv for EWS tools in $BASE_DIR/venv"
+  echo "Create virtualenv for EWS tools in $(pwd)/venv"
   exit 1
 fi
 
